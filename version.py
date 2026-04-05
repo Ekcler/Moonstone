26fbcef83d799563c91d1bc9d4680c35038286f4
Ekcler <arbyzbebrovich.12@gmail.com>
2026-04-05 16:24:21 +0300

    update version to 3.2.3

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 VSVersionInfo(
   ffi=FixedFileInfo(
-    filevers=(1, 2, 0, 0),
-    prodvers=(1, 2, 0, 0),
+    filevers=(3, 2, 3, 0),
+    prodvers=(3, 2, 3, 0),
     mask=0x3f,
     flags=0x0,
     OS=0x40004,
@@ -16,12 +16,12 @@ VSVersionInfo(
         u'040904B0',
         [StringStruct(u'CompanyName', u'Ekcler'),
         StringStruct(u'FileDescription', u'Sakura Flow DPI Bypass Application'),
-        StringStruct(u'FileVersion', u'1.2.0.0'),
+        StringStruct(u'FileVersion', u'3.2.3.0'),
         StringStruct(u'InternalName', u'SakuraFlow'),
         StringStruct(u'LegalCopyright', u'\xa9 Ekcler. All rights reserved.'),
         StringStruct(u'OriginalFilename', u'SakuraFlow.exe'),
         StringStruct(u'ProductName', u'Sakura Flow'),
-        StringStruct(u'ProductVersion', u'1.2.0.0')])
+        StringStruct(u'ProductVersion', u'3.2.3')])
       ]), 
     VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
   ]
